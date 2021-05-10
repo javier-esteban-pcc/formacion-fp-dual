44cde82f0a20c05df0286ecea24c49b6bdc7ee66
Javier Esteban <fjesteban@gmail.com>
2021-05-10 09:58:16 +0200

    install symfony/httpfoundation

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "Hola Mundo";
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+$data = [
+    1 => [
+        'id' => 1,
+        'name' => 'John',
+        'age' => 12
+    ]
+];
+
+$request = Request::createFromGlobals();
+
+$id= $request->get('id');
+
+$response =  new JsonResponse($data[$id]);
+$response->send();
