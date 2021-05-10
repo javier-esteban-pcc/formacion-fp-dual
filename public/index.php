9ef9cae51c4f9e23821252de38c5bd6df15b1ff2
Javier Esteban <fjesteban@gmail.com>
2021-05-10 10:50:33 +0200

    add JSON response example

--- public/index.php
+++ public/index.php
@@ -1,2 +1,18 @@
 <?php
-echo "Hola Mundo";
+
+$data = [
+    1 => [
+        'id' => 1,
+        'name' => 'John',
+        'age' => 22
+    ]
+];
+
+$id =  $_GET['id'] ??  null;
+
+if ($id === null || false === isset($data[$id])) {
+    http_response_code(404);
+} else {
+    header('Content-Type: application/json');
+    echo json_encode($data[$id]);
+}
