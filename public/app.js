2c6c815b1c1a26da4ab3b8440582e7f04dcc32d8
Javier Esteban <fjesteban@gmail.com>
2021-06-07 10:19:40 +0200

    init

--- public/app.js
+++
@@ -1,7 +0,0 @@
-import {getAllPosts} from "./js/postsApiClient.js";
-import {renderPosts, showNewPostModal} from "./js/postsTable.js";
-
-let newPostButton = document.getElementById('new-post')
-newPostButton.addEventListener('click', showNewPostModal);
-
-getAllPosts(renderPosts);
