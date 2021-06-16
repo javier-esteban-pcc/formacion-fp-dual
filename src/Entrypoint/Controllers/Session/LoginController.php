300343f4b65188c6ced8d059bba841f3a4de202e
Javier Esteban <fjesteban@gmail.com>
2021-06-16 14:50:32 +0200

    clean comment

--- src/Entrypoint/Controllers/Session/LoginController.php
+++ src/Entrypoint/Controllers/Session/LoginController.php
@@ -16,22 +16,6 @@ class LoginController
         $email = $request->get('email');
         $password = $request->get('password');
 
-//        $connection =  new \PDO("mysql:host=mysql;port=3306;dbname=posts","root","admin1234",[]);
-//
-//        $stmt = $connection->prepare('SELECT id, email, password, role FROM user WHERE email = :email');
-//
-//        $stmt->execute([':email' => $request->get('email')]);
-//
-//        if ($stmt->rowCount() === 0) {
-//            return new JsonResponse(['Invalid user or password'], Response::HTTP_UNAUTHORIZED);
-//        }
-//
-//        $result = $stmt->fetch(\PDO::FETCH_ASSOC);
-//
-//        if (password_verify($password, $result['password']) === false) {
-//            return new JsonResponse(['Invalid user or password'], Response::HTTP_UNAUTHORIZED);
-//        }
-
         $userRepository = new CsvUserRepository();
         $user = $userRepository->findByEmail($email);
 
